2d792dcb48983e898e59964c1ba981319381c816
santacrocec <cristian.santacroce@cg.tasgroup.eu>
2021-12-08 21:06:01 +0100

    entry and currency decoration
    flex layout wip

--- src/app/components/ccentry/ccentry.component.ts
+++ src/app/components/ccentry/ccentry.component.ts
@@ -1,4 +1,4 @@
-import { Component,  Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Entry } from 'src/app/models/Entry';
 import { Util } from 'src/app/Util/Util';
 
@@ -10,16 +10,35 @@ import { Util } from 'src/app/Util/Util';
 export class CcentryComponent implements OnInit {
   @Input() entry: Entry = new Entry;
 
-  perc! : number;
-  gainloss! : number;
+  perc!: number;
+  gainloss!: number;
+  staking!: String[];
 
 
-  constructor() { 
+  constructor() {
   }
 
   ngOnInit(): void {
     this.perc = Util.calculatePerc(this.entry.actualPrice, this.entry.price);
     this.gainloss = Util.calculateActualGainLoss(this.entry.qty, this.entry.actualPrice, this.entry.price);
+    this.staking = this.prepareStaking(this.entry.staking);
   }
+
+  getTrending(val: number): String {
+    return Util.prepareTrending(val);
+  }
+
+  prepareStaking(val: number): String[] {
+    switch (val) {
+      case 2:
+        return ["lock", "red"];
+      case 1:
+        return ["lock", "yellow"];
+      case 0:
+      default:
+        return ["lock_open", "green"];
+    }
+  }
+
 
 }
--- src/app/components/ccurrency/ccurrency.component.ts
+++ src/app/components/ccurrency/ccurrency.component.ts
@@ -4,7 +4,7 @@ import { InputEntry } from 'src/app/models/CCInput';
 import { CriptoCurrency } from 'src/app/models/CriptoCurrency';
 import { CurrencyDataService } from 'src/app/services/currency-data.service';
 import { Util } from 'src/app/Util/Util';
-import inputdata from  'src/assets/inputdata/input.json';
+import inputdata from 'src/assets/inputdata/input.json';
 
 @Component({
   selector: 'ccurrency',
@@ -16,31 +16,33 @@ import inputdata from  'src/assets/inputdata/input.json';
 export class CcurrencyComponent implements OnInit {
 
   @Input() showEntries!: boolean;
-  currencyList : Array<CriptoCurrency> = [];
+  currencyList: Array<CriptoCurrency> = [];
 
-  constructor(private service : CurrencyDataService) { }
+  constructor(private service: CurrencyDataService) { }
 
   ngOnInit(): void {
     this.prepareCurrencies();
   }
-  
+
   prepareCurrencies(): void {
     inputdata.ccdata.forEach(cc => {
-      this.service.getCurrencyData(cc.fiat, cc.cc).subscribe((ccList: CriptoCurrency[]) => 
-      {
-        ccList[0].entryList = Adapter.prepareEntryList(<InputEntry[]> cc.entryList, ccList[0].current_price);
-        ccList[0].percColor= Util.preparePercColor(ccList[0].price_change_percentage_24h);
-        ccList[0].trending= Util.prepareTrending(ccList[0].price_change_percentage_24h);
-        const avgData : number [] = Util.calculateBuyPriceAverage(<InputEntry[]> cc.entryList);
-        ccList[0].totalQty= avgData[0];
-        ccList[0].buyPriceAverage= avgData[1];
-        ccList[0].buyPriceAveragePerc= Util.calculatePerc(ccList[0].current_price, ccList[0].buyPriceAverage);
-        ccList[0].buyPriceAverageGainLoss= Util.calculateActualGainLoss(ccList[0].totalQty, ccList[0].current_price, ccList[0].buyPriceAverage);
+      this.service.getCurrencyData(cc.fiat, cc.cc).subscribe((ccList: CriptoCurrency[]) => {
+        ccList[0].entryList = Adapter.prepareEntryList(<InputEntry[]>cc.entryList, ccList[0].current_price);
+        ccList[0].percColor = Util.preparePercColor(ccList[0].price_change_percentage_24h);
+        ccList[0].trending = Util.prepareTrending(ccList[0].price_change_percentage_24h);
+        const avgData: number[] = Util.calculateBuyPriceAverage(<InputEntry[]>cc.entryList);
+        ccList[0].totalQty = avgData[0];
+        ccList[0].buyPriceAverage = avgData[1];
+        ccList[0].buyPriceAveragePerc = Util.calculatePerc(ccList[0].current_price, ccList[0].buyPriceAverage);
+        ccList[0].buyPriceAverageGainLoss = Util.calculateActualGainLoss(ccList[0].totalQty, ccList[0].current_price, ccList[0].buyPriceAverage);
         this.currencyList.push(ccList[0]);
       },
-      error => console.log(error.status)
+        error => console.log(error.status)
       );
     });
   }
-  
+
+  getTrending(val: number): String {
+    return Util.prepareTrending(val);
+  }
 }
